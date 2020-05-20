f143f3754a62fb1a797cf03f48e2ee7a36394f69
Andreas Kryger Jensen <aeje@sund.ku.dk>
2020-05-20 11:08:56 +0200

    updating simulations

--- simulations/simulate.R
+++ simulations/simulate.R
@@ -4,7 +4,7 @@ source("functions.R")
 
 set.seed(12345)
 
-R <- 10000
+R <- 10^4
 
 ########################################################
 #n = 25
@@ -33,9 +33,14 @@ sim25_4 <- mclapply(1:R, function(r) {
   doSim(25, 0.15)
 }, mc.cores = 64)
 
-save(sim25_1, sim25_2, sim25_3, sim25_4,
-     file="sim25_res.RData")
+set.seed(12345)
+sim25_5 <- mclapply(1:R, function(r) {
+  cat("Simulation", r, "\n")
+  doSim(25, 0.2)
+}, mc.cores = 64)
 
+save(sim25_1, sim25_2, sim25_3, sim25_4, sim25_5,
+     file="sim25_res.RData")
 
 ########################################################
 #n = 50
@@ -64,7 +69,13 @@ sim50_4 <- mclapply(1:R, function(r) {
   doSim(50, 0.15)
 }, mc.cores = 64)
 
-save(sim50_1, sim50_2, sim50_3, sim50_4,
+set.seed(12345)
+sim50_5 <- mclapply(1:R, function(r) {
+  cat("Simulation", r, "\n")
+  doSim(50, 0.2)
+}, mc.cores = 64)
+
+save(sim50_1, sim50_2, sim50_3, sim50_4, sim50_5,
      file="sim50_res.RData")
 
 ########################################################
@@ -94,5 +105,12 @@ sim100_4 <- mclapply(1:R, function(r) {
   doSim(100, 0.15)
 }, mc.cores = 64)
 
-save(sim100_1, sim100_2, sim100_3, sim100_4,
+set.seed(12345)
+sim100_5 <- mclapply(1:R, function(r) {
+  cat("Simulation", r, "\n")
+  doSim(100, 0.2)
+}, mc.cores = 64)
+
+save(sim100_1, sim100_2, sim100_3, sim100_4, sim100_5,
      file="sim100_res.RData")
+
